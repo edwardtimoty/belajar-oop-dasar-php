72195c3cf9374afd6164f0d60affb499f9ff1a7a
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-15 15:14:24 +0700

    belajar tentang class dan object
    15/4/2021

---
+++ 1. Class_dan_Object/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+// membuat class
+class Coba {
+
+}
+
+// membuat object instance dari class Coba
+$a = new Coba();
+$b = new Coba();
+
+var_dump($a);
+
+?>
---
+++ 1. Class_dan_Object/produk.php
@@ -0,0 +1,10 @@
+<?php 
+
+class Produk {
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+
+?>
