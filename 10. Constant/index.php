66f9d6c6c4a8f53dc1a7f50b021d5c72d2e1cd29
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-16 17:51:03 +0700

    belajar fungsi const dan define
    16/4/2021

---
+++ 10. Constant/index.php
@@ -0,0 +1,44 @@
+<?php 
+// constant = sebuah identifier untuk menyimpan nilai (nilainya tidak dapat berubah)
+// define(), const
+
+// membuat const harus dengan huruf besar semua
+// define tidak bisa simpan kedalam sebuah class harus disimpan diluar sebagai const global
+// define('NAMA', 'Edward');
+// echo NAMA;
+
+// echo "<br>";
+// // const bisa disimpan didalam class bisa digunakan dalam konsep OOP
+// const UMUR = 22;
+// echo UMUR;
+
+
+// class Coba
+// {
+//     const NAMA = 'Jeanice';
+// }
+// echo Coba::NAMA;
+
+
+// Magic Const
+// menampilkan baris dimana const ini ditulis
+// echo __LINE__;
+// menampilkan alamat dari const
+// echo __FILE__;
+
+// function coba()
+// {
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+// class Coba
+// {
+//     public $kelas = __CLASS__;
+// }
+
+// $obj = new Coba;
+// echo $obj->kelas;
+
+
+?>
